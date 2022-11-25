a37eef4bbc78497a5e95d4744f8dbc18c02c1aed
Kim <yichen.hsieh_c7_alumni@teach4taiwan.org>
2022-11-25 18:18:12 +0800

    click start-btn to go to select-charater-page

--- index.js
+++ index.js
@@ -1,24 +1,40 @@
 "use strict";
 
-function changeStage1() {
+// --- FUNCTION --- //
+// FN-1
+function readyToChangeStage() {
   const wholePage = document.querySelector("body");
-  const opening1 = document.querySelector(".opening-page-1");
-  const opening2 = document.querySelector(".opening-page-2");
 
   wholePage.addEventListener(
     "click",
     () => {
-      opening1.classList.add("remove");
-      opening2.classList.remove("remove");
+      changeStage1();
       setTimeout(changeStage2, 3000);
     },
     { once: true }
   );
 }
 
+// FN-2
+function changeStage1() {
+  const opening1 = document.querySelector(".opening-page-1");
+  const opening2 = document.querySelector(".opening-page-2");
+
+  opening1.classList.add("remove");
+  opening2.classList.remove("remove");
+}
+
+// FN-3
 function changeStage2() {
   document.querySelector(".talking").classList.add("remove");
   document.querySelector(".talking2").classList.remove("remove");
 }
-// 移除開場畫面
-setTimeout(changeStage1, 2000);
+
+// --- EVENT LISTENER --- //
+document.querySelector(".start-btn").addEventListener("click", () => {
+  window.location.assign("./select-character.html");
+});
+
+// --- EXECUTE --- //
+// 開場畫面兩秒後可以點擊轉場
+setTimeout(readyToChangeStage, 2000);
