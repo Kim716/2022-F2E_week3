cdbe3e7055b720e52790e38016527bf85d29971f
Kim <yichen.hsieh_c7_alumni@teach4taiwan.org>
2022-11-25 17:28:07 +0800

    click to change opening-page stage1

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 "use strict";
+
+function changeStage1() {
+  const wholePage = document.querySelector("body");
+  const opening1 = document.querySelector(".opening-page-1");
+  const opening2 = document.querySelector(".opening-page-2");
+
+  wholePage.addEventListener(
+    "click",
+    () => {
+      opening1.classList.add("remove");
+      opening2.classList.remove("remove");
+    },
+    { once: true }
+  );
+}
+// 移除開場畫面
+setTimeout(changeStage1, 2000);
