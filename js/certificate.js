2fd853d2c2239bb3edd632372ef5ab8cbf1855df
Kim <yichen.hsieh_c7_alumni@teach4taiwan.org>
2022-12-02 14:22:30 +0800

    Certificate-page: click continue-btn to see certificate

--- js/certificate.js
+++ js/certificate.js
@@ -1,1 +1,24 @@
 "use strict";
+
+// --- DOM --- //
+const continueBtn = document.querySelector(".continue-btn");
+
+const description = document.querySelector(".description-1");
+
+const certificateView = document.querySelector(".certificate-container");
+
+// --- FUNCTION --- //
+function toggleRemove(target) {
+  target.classList.toggle("remove");
+}
+
+// --- EVENTLISTENER --- //
+continueBtn.addEventListener("click", (e) => {
+  toggleRemove(description);
+  toggleRemove(certificateView);
+});
+// 按下一頁看證書
+// 抓名字、圖片
+// 渲染證書
+// 截圖
+// 回首頁
